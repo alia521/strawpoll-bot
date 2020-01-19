9e17747b4a3ef3faed82da98ad0410c1fc23ea76
HRPsomenumbers <42450617+HRPsomenumbers@users.noreply.github.com>
2020-01-19 20:43:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var plist   = require('./socks.json');
+var request = require('request'); // 
+var cheerio = require('cheerio'); //
+var plist   = require('./socks.json'); // HTTP proxies LIST
 
 var r = request.defaults()
+var x = 0; // number of proxies that didn't work for some reason;
+var y = 0; // number of proxies that have worked;
 
 
 
-function vote(url, value, n, proxy) {
+
+
+/* edit below */
+
+var vote_url = 'https://www.strawpoll.me/19271268' // edit last numbers
+var op = 0; // choose an option; 0 = first one, 1 = second, 2 = third and so on
+var ratio = 200; // ratio of votes; 200 = 5 per second; 1000/ratio = y per second;
+
+/* warning: do not make changes anywhere else if you do not know what you're doing*/
+
+
+
+
+
+function vote(url, value, n) { // function for vote
 request(url, function (error, response, body) {
 
   
@@ -18,10 +34,12 @@ request(url, function (error, response, body) {
 
   data = {"s":a+b, "opval":options}
 
-  for(i=0;i<n;i++) { 
-   setP(i)
-   sendVote(url, data.s, options.eq(value).val(), i) 
-    }
+  var i = 0; var x = plist.length;
+
+  function fx() {
+     setP(i); sendVote(url, data.s, options.eq(value).val()); i++; if(i < x ) { setTimeout(fx, ratio); }}
+  fx();
+
  })
 }
 
@@ -30,12 +48,14 @@ function setP(i) {
 }
 
 function sendVote(url, security_token, option_, a) {
-  r.post(url, {form:{"security-token":security_token, options:option_}}).on('error', function(err) {
-    console.log('[' + a + ']', 'Something went wrong. Testing the next proxy.');
- })	
+  r.post(url, {form:{"security-token":security_token, 'options':option_}},
+
+  function (error, response, body) {
+    if (error) { x++; } else { y++ }
+    console.log('Successfully sent vote', x, "times.", "Failed", y, "times.", "Tried", x+y, "proxies.")
+  })
 }
 
 
-vote('https://www.strawpoll.me/xxxxxxxx', 'OPTION you want to vote for. FIRST OPTION = 0, SECOND = 1',
-     plist.length, true)
+vote(vote_url, op, plist.length)
 
