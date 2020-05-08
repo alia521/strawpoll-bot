54b2a0161582bd4e5fcdb905891e9804ce711813
HRPsomenumbers <42450617+HRPsomenumbers@users.noreply.github.com>
2020-05-08 15:57:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var request = require('request'); // 
-var cheerio = require('cheerio'); //
-var plist   = require('./socks.json'); // HTTP proxies LIST
+var request     = require('request'); // 
+var cheerio     = require('cheerio'); //
+var fuck_errors = require('longjohn'); // supposed to handle error ECONNRESET
+var plist       = require('./socks.json'); // HTTP proxies LIST
+
 
 var r = request.defaults()
 var x = 0; // number of proxies that didn't work for some reason;
@@ -8,54 +10,40 @@ var y = 0; // number of proxies that have worked;
 
 
 
-
-
 /* edit below */
 
-var vote_url = 'https://www.strawpoll.me/19271268' // edit last numbers
-var op = 0; // choose an option; 0 = first one, 1 = second, 2 = third and so on
-var ratio = 200; // ratio of votes; 200 = 5 per second; 1000/ratio = y per second;
+var vote_url = 'https://www.strawpoll.me/19987846' // edit last numbers
+var op       = 0; // choose an option; 0 = first one, 1 = second, 2 = third and so on
+var ratio    = 10; // ratio of votes; 200 = 5 per second; 1000/ratio = y per second;
 
 /* warning: do not make changes anywhere else if you do not know what you're doing*/
-
-
-
 
 
 function vote(url, value, n) { // function for vote
 request(url, function (error, response, body) {
 
-  
-
   $ = cheerio.load(body); var data = $('form[data-form-type="poll-vote"]').html();
-  $ = cheerio.load(data); var a = $('input[id="field-security-token"]').val(); var b = $('input[id="field-authenticity-token"]').attr("name");
+  $ = cheerio.load(data); var a = $('input[id="field-security-token"]').val();
+  var b = $('input[id="field-authenticity-token"]').attr("name");
 
   var options = $('input[name="options"]');
-
   data = {"s":a+b, "opval":options}
 
   var i = 0; var x = plist.length;
 
-  function fx() {
-     setP(i); sendVote(url, data.s, options.eq(value).val()); i++; if(i < x ) { setTimeout(fx, ratio); }}
-  fx();
+  function fx() { setP(i); sendVote(url, data.s, options.eq(value).val()); i++; if(i < x ) { setTimeout(fx, ratio); }} 
+           fx();
 
- })
-}
+ })}
 
-function setP(i) {
-  r = request.defaults({'proxy':'http://' + plist[i + 0]})
-}
+function setP(i) {  r = request.defaults({'proxy':'http://' + plist[i + 0]}) }
 
 function sendVote(url, security_token, option_, a) {
-  r.post(url, {form:{"security-token":security_token, 'options':option_}},
-
-  function (error, response, body) {
-    if (error) { x++; } else { y++ }
-    console.log('Successfully sent vote', x, "times.", "Failed", y, "times.", "Tried", x+y, "proxies.")
-  })
-}
+  r.post(url, {form:{"security-token":security_token, 'options':option_}}).on('error', function (exc) {
+   x++
+}).on('response', function(response) { if(response.statusCode == 200) { y++ } else { x++}
+                                       console.log(y, "succesfuly votes made.", x, 'failed.');
+})}
 
 
 vote(vote_url, op, plist.length)
-
